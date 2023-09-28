542c72e43ef75a8ef0e022667806cf55c2c163fc
Vanshika Poojari <140260623+vanshika-poojari@users.noreply.github.com>
2023-09-28 14:35:27 +0530

    quiz.js

--- quiz.js
+++ quiz.js
@@ -1,21 +1,129 @@
-var div = document.getElementById("firstdiv");
+const questionbox = document.getElementById("questionBox")
+const choicesbox = document.getElementById("choices")
+const userScore = document.getElementById("user")
+const startQuiz = document.getElementById("buttons")
 
-div.innerHTML = "<b> <p style = 'margin-top: 10px' > Coding Quiz Challenge</b> </p> <br>  Try to answer the following code-relatd questions within the time limit <br> <p style = 'margin-top: 10px'> Keep in mind that incorrect answers will penalize your score time by ten seconds </p>"; 
-const btn = document.createElement("button");
-
-btn.innerHTML = "Start Quiz";
-btn.style.cursor = "pointer";
-
-btn.style.marginleft = "100%";
+let score = 0; 
 
 
-btn.addEventListener("click", function() {
+// Making an array storing questions choices and answers
+const quizbox = [
+    {
+        question: " Q1.  Commonly used data types DO NOT include: ",
+        choices:["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        question: "Q2. Which of the following is not a CSS box model property? ",
+        choices:["margin", "padding", "border-radius", "border-collapse"],
+        answer: "border-collapse"
+    },
+    {
+        question: "Q3. Which of the following is not a JavaScript data type? ",
+        choices:["string", "boolean", "objects", "float"],
+        answer: "float"
+    },
+    {
+        question: "Q4. What is the purpose of the this keyword in JavaScript ",
+        choices:["it refers to the current function", "it refers to the current object", "it refers to any object", "it refers to a memory location"],
+        answer: "it refers to the current object"
+    }
+];
 
-var textonClick = "<b>Commonly used data types DO NOT include : </b> <br> <br> <p style = 'border: 10px solid teal; background-color: teal; border-radius: 1em'> 1. strings </p> <br><br> <p style = 'border: 10px solid teal; background-color: teal; border-radius: 1em'> 2. booleans </p> <br><br> <p style = 'border: 10px solid teal; background-color: teal; border-radius: 1em'> 3. alerts </p> <br><br> <p style = 'border: 10px solid teal; background-color: teal; border-radius: 1em'> 4. numbers "
 
-div.innerHTML = textonClick;
+
+let currentQuestionIndex = 0; 
+//function to show questions 
+function showQuestions(){ 
+    // alert ("clicked");
+    const firstQuestion = quizbox[currentQuestionIndex];
+   questionbox.textContent = firstQuestion.question;
+
+   choicesbox.textContent = "";
+   for(let i = 0; i<firstQuestion.choices.length; i++){
+    const currentchoice = firstQuestion.choices[i];
+    const choicediv = document.createElement('div');
+    choicediv.textContent = currentchoice; 
+    choicediv.classList.add('choice'); 
+    choicesbox.appendChild(choicediv);
+
+choicediv.addEventListener('click', () => {
+if (choicediv.classList.contains('selected')){
+    choicediv.classList.remove('selected')
+}
+else{
+    choicediv.classList.add('selected')
+}
 
 })
 
-document.body.appendChild(btn);
 
+   }
+
+
+}
+// function checkAnswer(){
+//     const selectedChoice = document.querySelector('.choice.selected');
+//     if (selectedChoice.textContent === quizbox[currentQuestionIndex].answer){
+//         alert("right answer")        
+//         score++;
+//     }
+//     else{
+//         alert("wrong answer")
+//     }
+function checkAnswer() {
+
+    const selectedChoice = document.querySelector(".choice.selected");
+    if (selectedChoice.textContent === quizbox[currentQuestionIndex].answer) {
+      selectedChoice.style.backgroundColor = "green";
+      score++;
+      preventdefault();
+    } else {
+      selectedChoice.style.backgroundColor = "red";
+      preventdefault();
+    }
+    currentQuestionIndex++;
+    if(currentQuestionIndex<quizbox.length){
+        showQuestions();
+    } else{
+        checkScore();
+    }
+  
+}
+
+//function to show score
+
+const checkScore = () => {
+    questionbox.textContent = "";
+    choicesbox.textContent= "";
+    userScore.textContent = ` You scored ${score} out of ${quizbox.length}`
+    startQuiz.textContent = "Play Again!";
+    startQuiz.addEventListener('click', function(){
+        currentQuestionIndex = 0; 
+        showQuestions();
+    })
+}
+
+
+showQuestions(); 
+startQuiz.addEventListener("click", function(){
+    startQuiz.innerHTML = "Next"
+    checkAnswer();
+    
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+// function beginQuiz(){
+//     questionAsked.innerHTML = questions[currentQuestion].question;
+    
+// }
